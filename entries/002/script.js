53fc4ac13739f38cf688f22b7ec9fba45d485581
aniqatc <aniqatc97@gmail.com>
2023-10-04 02:36:05 -0400

    Day 002: refactor

--- entries/002/script.js
+++ entries/002/script.js
@@ -3,9 +3,11 @@ const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', () => {
 	if (hamburger.classList.contains('active')) {
 		hamburger.classList.remove('active');
+		void hamburger.offsetWidth;
 		hamburger.classList.add('active-reverse');
 	} else {
 		hamburger.classList.remove('active-reverse');
+		void hamburger.offsetWidth;
 		hamburger.classList.add('active');
 	}
 });
