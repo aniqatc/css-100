d6bf063a1e84c1e90994e10637dd32f6fed999d2
aniqatc <aniqatc97@gmail.com>
2023-10-10 21:47:59 -0400

    minor styling updates

--- entries/010/script.js
+++ entries/010/script.js
@@ -3,6 +3,7 @@
 const date = document.getElementById('date');
 const time = document.getElementById('time');
 const calories = document.getElementById('calories');
+const heartRate = document.getElementById('bpm');
 
 const current = new Date();
 const minutes = String(current.getMinutes());
@@ -21,4 +22,5 @@ date.innerHTML = `${current.toDateString()}`;
 time.innerHTML = `${formatHours(current.getHours())}:${
 	minutes.length === 2 ? minutes : '0' + minutes
 }`;
-calories.innerHTML = `${Math.floor(Math.random() * 1200) + 600} KCAL`;
+calories.innerHTML = `<b>${Math.floor(Math.random() * 1200) + 600}</b> CAL`;
+heartRate.innerHTML = ` ${Math.floor(Math.random() * 100) + 65}`;
