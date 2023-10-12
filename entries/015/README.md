886ab6de5e721de774359649e251bb80dc7307ee
aniqatc <aniqatc97@gmail.com>
2023-10-12 19:57:15 -0400

    day 015: initial upload of code

---
+++ entries/015/README.md
@@ -0,0 +1,7 @@
+# Day 015
+
+**Time to Complete**: 45 mins
+
+**Key Concepts**: custom file upload input, accessing file input elements in JavaScript
+
+**Notes**:
---
+++ entries/015/script.js
@@ -0,0 +1,27 @@
+const dropzone = document.querySelector('.dropzone');
+const fileInput = document.querySelector('#file-input');
+const fileUpload = document.querySelector('.file-upload');
+const progressBar = document.querySelector('.progress');
+
+fileInput.addEventListener('change', event => {
+	const fileName = event.target.files[0].name;
+	dropzone.textContent = `${fileName}`;
+});
+
+fileUpload.addEventListener('click', () => {
+	if (fileInput.files.length > 0) {
+		progressBar.classList.add('active');
+		dropzone.style.border = `0`;
+		dropzone.innerHTML = `
+					<img src="/entries/015/images/sync.svg" class="sync">
+	`;
+		fileUpload.textContent = 'Uploading...';
+		fileUpload.style.boxShadow = '0 1px 10px #6ece3b';
+
+		setTimeout(() => {
+			dropzone.innerHTML = `<img src="/entries/015/images/check.svg" class="complete">`;
+			fileUpload.style.boxShadow = 'none';
+			fileUpload.textContent = 'Upload Complete';
+		}, 2000);
+	}
+});
