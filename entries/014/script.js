ffe650d238d619398dbaedc74ba80fe7448f5c28
aniqatc <aniqatc97@gmail.com>
2023-10-12 01:31:28 -0400

    day 013: improve script - 1 func for both animations

--- entries/014/script.js
+++ entries/014/script.js
@@ -1,34 +1,21 @@
-/* ANIMATION FOR BIKE GROUND */
+function insertAnimatedLines(container, dashNum) {
+	for (let i = 0; i < dashNum; i++) {
+		const span = document.createElement('span');
+		span.className = 'dash';
+		document.querySelector(`.${container}`).appendChild(span);
+	}
 
-const ground = document.querySelector('.ground');
+	document.querySelectorAll(`.${container} .dash`).forEach(dash => {
+		dash.style.left = `${Math.random() * 200}px`;
+		dash.style.animationDelay = `${Math.random() * 300}ms`;
+		dash.style.width = `${5 + Math.random() * 7}px`;
+		dash.style.animationDuration = `${400 + Math.random() * 900}ms`;
 
-for (let i = 0; i < 12; i++) {
-	const span = document.createElement('span');
-	span.className = 'dash';
-	ground.appendChild(span);
+		if (container === 'sky') {
+			dash.style.top = `${Math.random() * 200}px`;
+		}
+	});
 }
 
-document.querySelectorAll('.ground .dash').forEach(dash => {
-	dash.style.left = `${Math.random() * 180}px`;
-	dash.style.animationDelay = `${Math.random() * 300}ms`;
-	dash.style.width = `${5 + Math.random() * 12}px`;
-	dash.style.animationDuration = `${400 + Math.random() * 900}ms`;
-});
-
-/* ANIMATION FOR HELICOPTER SKY */
-
-const sky = document.querySelector('.sky');
-
-for (let i = 0; i < 20; i++) {
-	const span = document.createElement('span');
-	span.className = 'dash';
-	sky.appendChild(span);
-}
-
-document.querySelectorAll('.sky .dash').forEach(dash => {
-	dash.style.left = `${Math.random() * 200}px`;
-	dash.style.animationDelay = `${Math.random() * 300}ms`;
-	dash.style.width = `${5 + Math.random() * 12}px`;
-	dash.style.animationDuration = `${400 + Math.random() * 900}ms`;
-	dash.style.top = `${Math.random() * 140}px`;
-});
+insertAnimatedLines('sky', 24);
+insertAnimatedLines('ground', 12);
