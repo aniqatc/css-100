df1fed885756989ee8913299cbb33ee4cbf9a57c
aniqatc <aniqatc97@gmail.com>
2023-10-26 01:35:05 -0400

    day 025: initial markup & styling

---
+++ entries/025/script.js
@@ -0,0 +1,16 @@
+const marker = document.querySelector('.day-025 .marker');
+const shadow = document.querySelector('.day-025 .shadow');
+const card = document.querySelector('.day-025 .card');
+const links = document.querySelector('.day-025 .card-content__links');
+
+marker.addEventListener('click', () => {
+	marker.classList.add('active');
+	shadow.classList.add('active');
+	card.classList.add('active');
+});
+
+links.addEventListener('click', () => {
+	marker.classList.remove('active');
+	shadow.classList.remove('active');
+	card.classList.remove('active');
+});
