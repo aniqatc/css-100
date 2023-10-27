3f3a2a06730e68f39325b9c404bfec7f9f50ad32
aniqatc <aniqatc97@gmail.com>
2023-10-27 01:33:13 -0400

    day 026: initial markup & styling

---
+++ entries/026/script.js
@@ -0,0 +1,14 @@
+(function () {
+	const modals = document.querySelectorAll('.day-026 .modal');
+	const buttons = document.querySelectorAll('.day-026 .button');
+
+	buttons.forEach((button, index) => {
+		button.addEventListener('click', () => {
+			modals[index].classList.remove('active');
+			modals[index].classList.add('inactive');
+			/* original formula: modals[index === 2 ? index - 2 : index + 1] */
+			modals[(index + 1) % modals.length].classList.add('active');
+			modals[(index + 1) % modals.length].classList.remove('inactive');
+		});
+	});
+})();
