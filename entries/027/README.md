c7a1193bf2906f4e5951357590c2474134b92c73
aniqatc <aniqatc97@gmail.com>
2023-10-28 00:25:09 -0400

    day 027: initial markup & styling

---
+++ entries/027/README.md
@@ -0,0 +1,9 @@
+# Day 027
+
+**<a href="https://css100.aniqa.dev#day-027">Live Demo</a>**
+
+**Time to Complete**: 45 mins
+
+**Key Concepts**: multiple labels for checkbox input, polyline svg & calculate stroke-dasharray
+
+**Notes**:
---
+++ entries/027/script.js
@@ -0,0 +1,18 @@
+(function () {
+	const cardHeader = document.querySelector('.day-027 .card-header');
+	const today = new Date();
+	const locale = navigator.geolocation;
+	const weekday = {
+		weekday: 'long',
+	};
+	const date = {
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+	};
+
+	cardHeader.innerHTML = `
+		<h1>${today.toLocaleDateString(locale, weekday)}</h1>
+		<p>${today.toLocaleDateString(locale, date)}</p>
+`;
+})();
