2686907465b265b2c2a0aef2f461d2c502d950aa
aniqatc <aniqatc97@gmail.com>
2023-10-30 00:27:53 -0400

    day 029: improve script

--- entries/029/script.js
+++ entries/029/script.js
@@ -1,15 +1,17 @@
-const searchInput = document.querySelector('.search-input');
-const suggestionsList = document.querySelector('.suggestions');
+(function () {
+	const searchInput = document.querySelector('.day-029 .search-input');
+	const suggestionsList = document.querySelector('.day-029 .suggestions');
 
-searchInput.addEventListener('input', event => {
-	const inputValue = event.target.value;
-	if (inputValue) {
-		suggestionsList.innerHTML = `
+	searchInput.addEventListener('input', event => {
+		const inputValue = event.target.value;
+		if (inputValue) {
+			suggestionsList.innerHTML = `
 			<li><strong>${inputValue}</strong></li>
 			<li>Veritatis et <strong>${inputValue}</strong></li>
 			<li>Ut aliquid ex <strong>${inputValue}</strong> veroe eos</li>
 	`;
-	} else {
-		suggestionsList.innerHTML = '';
-	}
-});
+		} else {
+			suggestionsList.innerHTML = '';
+		}
+	});
+})();
