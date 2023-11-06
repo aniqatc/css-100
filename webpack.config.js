284277b45b400c73434639121cf3f1ec0eecdac1
aniqatc <aniqatc97@gmail.com>
2023-11-05 23:36:34 -0500

    webpack: add image-webpack-loader for image optimization

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,27 @@ module.exports = {
 				generator: {
 					filename: 'assets/[name][ext]',
 				},
+				use: [
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+								progressive: true,
+								quality: 65,
+							},
+							optipng: {
+								enabled: false,
+							},
+							pngquant: {
+								quality: [0.65, 0.9],
+								speed: 4,
+							},
+							webp: {
+								quality: 75,
+							},
+						},
+					},
+				],
 			},
 		],
 	},
