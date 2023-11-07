013fbcd782a469cb21343c84c5f390fb9ebc6c3b
aniqatc <aniqatc97@gmail.com>
2023-11-07 04:19:17 -0500

    day 013: remove avif images to improve performance

--- entries/013/content.js
+++ entries/013/content.js
@@ -1,8 +1,3 @@
-import Scene1AVIF from '/entries/013/images/scene-1.avif';
-import Scene2AVIF from '/entries/013/images/scene-2.avif';
-import Scene3AVIF from '/entries/013/images/scene-3.avif';
-import Scene4AVIF from '/entries/013/images/scene-4.avif';
-import AvatarAVIF from '/entries/013/images/avatar.avif';
 import Scene1WebP from '/entries/013/images/scene-1.webp';
 import Scene2WebP from '/entries/013/images/scene-2.webp';
 import Scene3WebP from '/entries/013/images/scene-3.webp';
@@ -24,64 +19,48 @@ export function getMarkup() {
 	<div class="wrapper">
 		<div class="gallery">
 			<div class="gallery-image">
-				<picture>
-					<source srcset="${Scene1AVIF}" />
 					<img
 						src="${Scene1WebP}"
 						alt="gallery image, rocky walkway in nature"
-				/></picture>
+				/>
 				<span><i class="fa-solid fa-plus open-icon"></i></span>
 			</div>
 			<div class="gallery-image">
-				<picture>
-					<source srcset="${Scene2AVIF}" />
 					<img
 						src="${Scene2WebP}"
 						alt="gallery image, trees near a mansion"
 					/>
-				</picture>
 				<span><i class="fa-solid fa-plus open-icon"></i></span>
 			</div>
 
 			<div class="gallery-image">
-				<picture>
-					<source srcset="${Scene3AVIF}" />
 					<img
 						src="${Scene3WebP}"
 						alt="gallery image, house near mountains"
 					/>
-				</picture>
 				<span><i class="fa-solid fa-plus open-icon"></i></span>
 			</div>
 			<div class="gallery-image">
-				<picture>
-					<source srcset="${Scene4AVIF}" />
 					<img
 						src="${Scene4WebP}"
 						alt="gallery image, nature landscape"
 					/>
-				</picture>
 				<span><i class="fa-solid fa-plus open-icon"></i></span>
 			</div>
 		</div>
 		<div class="profile">
 			<div>
-				<picture>
-					<source srcset="${Scene4AVIF}" />
 					<img
 						src="${Scene4WebP}"
 						class="profile-cover"
 						alt="profile cover example, nature landscape"
 					/>
-				</picture>
 				<span><i class="fa-solid fa-xmark close-icon"></i></span>
-				<picture>
-					<source srcset="${AvatarAVIF}" />
 					<img
 						src="${AvatarWebP}"
 						class="profile-avatar"
 						alt="profile avatar example, back of a woman in coat and backpack"
-				/></picture>
+				/>
 			</div>
 			<div class="profile-bottom">
 				<span><b>Julia Toth</b> Photographer</span>
