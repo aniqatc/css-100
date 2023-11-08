0f2968f651a8d2bae4831e03787a3d9929342c10
aniqatc <aniqatc97@gmail.com>
2023-11-07 21:26:23 -0500

    day 037: initial markup and styling

---
+++ entries/037/README.md
@@ -0,0 +1,9 @@
+## Day 037
+
+**<a href="https://css100.aniqa.dev#day-037">Live Demo</a>**
+
+**Time to Complete**: 45 mins
+
+**Key Concepts**: 3D transforms for carousel animation
+
+**Notes**:
---
+++ entries/037/content.js
@@ -0,0 +1,56 @@
+export function getMarkup() {
+	return `
+<!-- Day 037 -->
+	<div class="day-037 frame" id="day-037">
+		<span class="code-link">
+			<a
+				href="https://github.com/aniqatc/css-100/tree/main/entries/037"
+				target="_blank"
+			    rel="noreferrer"
+				>Day 037: Carousel</a
+			>
+		</span>
+		<div class="center">
+		<div class="card">
+			<div class="top"></div>
+			<div class="content">
+				<span class="line line-1"></span>
+				<span class="line line-2"></span>
+				<span class="line line-3"></span>
+				<span class="line line-4"></span>
+                <span class="line line-1"></span>
+                <span class="line line-3"></span>
+				<span class="line line-2"></span>
+                <span class="line line-5"></span>
+			</div>
+		</div>
+		<div class="card">
+			<div class="top"></div>
+			<div class="content">
+				<span class="line line-1"></span>
+				<span class="line line-2"></span>
+				<span class="line line-3"></span>
+				<span class="line line-4"></span>
+                <span class="line line-1"></span>
+                <span class="line line-3"></span>
+				<span class="line line-2"></span>
+                <span class="line line-5"></span>
+			</div>
+		</div>
+		<div class="card">
+			<div class="top"></div>
+			<div class="content">
+				<span class="line line-1"></span>
+				<span class="line line-2"></span>
+				<span class="line line-3"></span>
+				<span class="line line-4"></span>
+                <span class="line line-1"></span>
+                <span class="line line-3"></span>
+				<span class="line line-2"></span>
+                <span class="line line-5"></span>
+			</div>
+		</div>
+		</div>
+	</div>  
+`;
+}
