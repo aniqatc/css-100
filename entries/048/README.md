18a7fb89c0447418a9c7d3f494368de7a7617371
aniqatc <aniqatc97@gmail.com>
2023-11-20 19:09:42 -0500

    day 048: initial markup & styling

---
+++ entries/048/README.md
@@ -0,0 +1,13 @@
+## Day 048
+
+**<a href="https://css100.aniqa.dev#day-048">Live Demo</a>**
+
+**Time to Complete**: 30 mins
+
+**Key Concepts**: 3D cube with transformations & perspective property
+
+**Notes**:
+
+## Reference
+
+<a href="https://3dtransforms.desandro.com/cube">Intro to CSS 3D Transforms: Cube by David DeSandro</a>
---
+++ entries/048/content.js
@@ -0,0 +1,25 @@
+export function getMarkup() {
+	return `
+<!-- Day 048 -->
+    <div class="day-048 frame" id="day-048">
+		<span class="code-link">
+			<a
+				href="https://github.com/aniqatc/css-100/tree/main/entries/048"
+				target="_blank"
+				rel="noreferrer"
+				>Day 048: 3D Cube</a
+			>
+		</span>
+<div class="center">
+		<div class="cube">
+			<div class="cube-face cube--front"></div>
+			<div class="cube-face cube--back"></div>
+			<div class="cube-face cube--right"></div>
+			<div class="cube-face cube--left"></div>
+			<div class="cube-face cube--top"></div>
+			<div class="cube-face cube--bottom"></div>
+		</div>
+	</div>
+</div>  
+`;
+}
