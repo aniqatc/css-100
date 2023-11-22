8cf1e9ce3a159120e9ce41210d5972ee15276bbc
aniqatc <aniqatc97@gmail.com>
2023-11-22 02:40:19 -0500

    day 050: initial markup & styling

---
+++ entries/050/README.md
@@ -0,0 +1,9 @@
+## Day 050
+
+**<a href="https://css100.aniqa.dev#day-050">Live Demo</a>**
+
+**Time to Complete**: 25 mins
+
+**Key Concepts**: border-radius, multi-step animation
+
+**Notes**:
---
+++ entries/050/content.js
@@ -0,0 +1,16 @@
+export function getMarkup() {
+	return `
+<!-- Day 050 -->
+	<div class="day-050 frame" id="day-050">
+		<span class="code-link">
+			<a
+				href="https://github.com/aniqatc/css-100/tree/main/entries/050"
+				target="_blank"
+				rel="noreferrer"
+				>Day 050: Easter Egg</a
+			>
+		</span>
+    <div class="egg"></div>
+</div>  
+`;
+}
