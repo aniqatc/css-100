3bfbc037d777b8eb140b5d49e119b94399f3d752
aniqatc <aniqatc97@gmail.com>
2023-11-24 05:56:15 -0500

    load.js: improve file loading option for SCSS & CSS

--- src/scripts/load.js
+++ src/scripts/load.js
@@ -1,12 +1,12 @@
 import '/src/styles/reset.css';
 import '/src/styles/main.css';
 
-const contentContext = require.context('/entries', true, /content\.js$/);
+const markupContext = require.context('/entries', true, /content\.js$/);
 const styleCSSContext = require.context('/entries', true, /style\.css$/);
 const styleSCSSContext = require.context('/entries', true, /style\.scss$/);
 
 async function loadContent(entry) {
-	const module = contentContext(`./${entry}/content.js`);
+	const module = markupContext(`./${entry}/content.js`);
 	const markup = module.getMarkup();
 	const container = document.querySelector('.container');
 	container.insertAdjacentHTML('beforeend', markup);
@@ -37,11 +37,10 @@ function scrollToHash() {
 (async function () {
 	for (let i = 51; i > 0; i--) {
 		const entry = String(i).padStart(3, '0');
-		if (i < 51 && i > 0) {
-			styleCSSContext(`./${entry}/style.css`);
-		} else if (i > 51 && i < 100) {
-			styleSCSSContext(`./${entry}/style.scss`);
-		}
+
+		// Load SCSS files for entries above 50
+		i > 50 ? styleSCSSContext(`./${entry}/style.scss`) : styleCSSContext(`./${entry}/style.css`);
+
 		await loadContent(entry);
 		await loadScript(entry);
 	}
