914b66d68173549e00f0face03812eb6a74eee09
aniqatc <aniqatc97@gmail.com>
2023-11-28 08:05:28 -0500

    day 057: initial markup & styling

---
+++ entries/057/README.md
@@ -0,0 +1,9 @@
+## Day 057
+
+**<a href="https://css100.aniqa.dev#day-057">Live Demo</a>**
+
+**Time to Complete**: 1.5 hours
+
+**Key Concepts**: icosahedron 3D shape
+
+**Notes**:
---
+++ entries/057/content.js
@@ -0,0 +1,39 @@
+export function getMarkup() {
+	return `
+<!-- Day 057 -->
+    <div class="day-057 frame" id="day-057">
+	    <span class="code-link">
+			<a
+			    href="https://github.com/aniqatc/css-100/tree/main/entries/057"
+				target="_blank"
+				rel="noreferrer"
+				>Day 057: Icosahedron</a
+			>
+		</span>
+	<div class="center">
+		<div class="shape">
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+			<div class="side"></div>
+		</div>
+	</div>
+</div>    
+ `;
+}
