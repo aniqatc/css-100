b3aba9c65c9a986fd76e544e3d53bb33e937e8ab
aniqatc <aniqatc97@gmail.com>
2023-11-28 03:13:38 -0500

    day 056: initial markup & styling

---
+++ entries/056/README.md
@@ -0,0 +1,9 @@
+## Day 056
+
+**<a href="https://css100.aniqa.dev#day-056">Live Demo</a>**
+
+**Time to Complete**: 1 hour
+
+**Key Concepts**: using @for loop to dynamically create individual @keyframe animations, staggered elements with rotation()
+
+**Notes**:
---
+++ entries/056/content.js
@@ -0,0 +1,40 @@
+export function getMarkup() {
+	return `
+<!-- Day 056 -->
+<div class="day-056 frame" id="day-056">
+	<span class="code-link">
+		<a
+		    href="https://github.com/aniqatc/css-100/tree/main/entries/056"
+			target="_blank"
+			rel="noreferrer"
+			>Day 056: Flower</a
+		>
+	</span>
+<div class="center">
+		<div class="flower">
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="petal"></div>
+			<div class="stigma"></div>
+		</div>
+	</div>
+</div>  
+ `;
+}
