664c115a75cbedd3ec3d837621767bd24cfb8493
aniqatc <aniqatc97@gmail.com>
2023-11-27 21:12:23 -0500

    day 055: initial markup & styling

---
+++ entries/055/README.md
@@ -0,0 +1,9 @@
+## Day 055
+
+**<a href="https://css100.aniqa.dev#day-055">Live Demo</a>**
+
+**Time to Complete**: 40 mins
+
+**Key Concepts**: conic-gradient(), hover transitions
+
+**Notes**:
---
+++ entries/055/content.js
@@ -0,0 +1,25 @@
+export function getMarkup() {
+	return `
+<!-- Day 055 -->
+    <div class="day-055 frame" id="day-055">
+		<span class="code-link">
+			<a
+				href="https://github.com/aniqatc/css-100/tree/main/entries/055"
+				target="_blank"
+				rel="noreferrer"
+				>Day 055: Thermostat</a
+			>
+		</span>
+		<div class="center">
+		<div class="gradient"></div>
+		<div class="temp-details">
+			<div class="indicator"></div>
+			<p class="temp--high">24°</p>
+			<p class="temp--current">18<span>°</span></p>
+			<p class="temp--current temp--next">23<span>°</span></p>
+			<p class="temp--location">Bedroom</p>
+		</div>
+	</div>
+</div>  
+`;
+}
