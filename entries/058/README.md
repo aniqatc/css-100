9bfae8ec152cf7aa26c7373dd45298204b6abd5f
aniqatc <aniqatc97@gmail.com>
2023-11-30 04:30:42 -0500

    day 058: initial markup & styling

---
+++ entries/058/README.md
@@ -0,0 +1,14 @@
+## Day 058
+
+**<a href="https://css100.aniqa.dev#day-058">Live Demo</a>**
+
+**Time to Complete**: 2 hours
+
+**Key Concepts**: using @mixins for repeating properties with different values, reverse-engineer original code to get formula for each sphere slice
+
+**Notes**:
+
+## References
+
+- <a href="https://codepen.io/roydigerhund/pen/GZMLbK">Codepen by Matthias Martin: Original 3D Sphere for Day 58</a>
+- <a href="https://observablehq.com/@stroked/how-to-slice-a-sphere">How To Slice A Sphere by Observable HQ</a>
---
+++ entries/058/content.js
@@ -0,0 +1,35 @@
+export function getMarkup() {
+	return `<!-- Day 058 -->
+	<div class="day-058 frame" id="day-058">
+		<span class="code-link">
+			<a
+			    href="https://github.com/aniqatc/css-100/tree/main/entries/058"
+			    target="_blank"
+			    rel="noreferrer"
+			        >Day 058: 3D Sliced Sphere</a
+			>
+		</span>
+	<div class="center">
+		<div class="slice slice-1"></div>
+		<div class="slice slice-2"></div>
+		<div class="slice slice-3"></div>
+		<div class="slice slice-4"></div>
+		<div class="slice slice-5"></div>
+		<div class="slice slice-6"></div>
+		<div class="slice slice-7"></div>
+		<div class="slice slice-8"></div>
+		<div class="slice slice-9"></div>
+		<div class="slice slice-10"></div>
+		<div class="slice slice-11"></div>
+		<div class="slice slice-12"></div>
+		<div class="slice slice-13"></div>
+		<div class="slice slice-14"></div>
+		<div class="slice slice-15"></div>
+		<div class="slice slice-16"></div>
+		<div class="slice slice-17"></div>
+		<div class="slice slice-18"></div>
+		<div class="slice slice-19"></div>
+	</div>
+</div>  
+`;
+}
