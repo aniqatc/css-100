da5b2b47b2d2cb0c56d1f8f6bba4181480de11ac
aniqatc <aniqatc97@gmail.com>
2023-12-02 14:56:29 -0500

    day 061: initial markup & styling

---
+++ entries/061/README.md
@@ -0,0 +1,9 @@
+## Day 061
+
+**<a href="https://css100.aniqa.dev#day-061">Live Demo</a>**
+
+**Time to Complete**: 5 mins
+
+**Key Concepts**: background gradient animation
+
+**Notes**:
---
+++ entries/061/content.js
@@ -0,0 +1,14 @@
+export function getMarkup() {
+	return `
+<!-- Day 061 -->
+    <div class="day-061 frame" id="day-061">
+		<span class="code-link">
+			<a
+				href="https://github.com/aniqatc/css-100/tree/main/entries/061"
+				target="_blank"
+				rel="noreferrer"
+				>Day 061: Gradient Transition</a
+			>
+		</span>
+</div>`;
+}
