36dbce5f32faa0aae89c9e726673c0cef51631ad
aniqatc <aniqatc97@gmail.com>
2023-12-03 12:08:11 -0500

    day 062: initial markup & styling

---
+++ entries/062/README.md
@@ -0,0 +1,9 @@
+## Day 062
+
+**<a href="https://css100.aniqa.dev#day-062">Live Demo</a>**
+
+**Time to Complete**: 30 mins
+
+**Key Concepts**: flex layouts, triggering different data on hover
+
+**Notes**:
---
+++ entries/062/content.js
@@ -0,0 +1,79 @@
+export function getMarkup() {
+	return `
+<!-- Day 062 -->
+    <div class="day-062 frame" id="day-062">
+		<span class="code-link">
+			<a
+				href="https://github.com/aniqatc/css-100/tree/main/entries/062"
+				target="_blank"
+				rel="noreferrer"
+				>Day 062: Pricing Table</a
+			>
+		</span>
+        	<div class="content">
+		<div class="pricing pricing-basic">
+			<div class="pricing--heading">Basic</div>
+			<div class="pricing--value">
+				<span>$5</span>
+				<span>per month</span>
+			</div>
+			<div class="pricing--content">
+				<span></span>
+				<span></span>
+				<span></span>
+                <span></span>
+			</div>
+		</div>
+		<div class="pricing pricing-pro">
+			<div class="pricing--heading">Pro</div>
+			<div class="pricing--value">
+				<span>$10</span>
+				<span>per month</span>
+			</div>
+			<div class="pricing--content">
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+		<div class="pricing pricing-premium">
+			<div class="pricing--heading">Premium</div>
+			<div class="pricing--value">
+				<span>$20</span>
+				<span>per month</span>
+			</div>
+			<div class="pricing--content">
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+		<div class="data">
+			<div class="data--users">
+				<p>5 Users</p>
+				<p>100 Users</p>
+				<span class="bar bar-users">
+					<span class="fill"></span>
+				</span>
+			</div>
+			<div class="data--gb">
+				<p>20 GB</p>
+				<p>200 GB</p>
+				<span class="bar bar-gb">
+					<span class="fill"></span>
+				</span>
+			</div>
+			<div class="data--project">
+				<p>1 Project</p>
+				<p>50 Projects</p>
+				<span class="bar bar-projects">
+					<span class="fill"></span>
+				</span>
+			</div>
+		</div>
+	</div>
+</div>  
+`;
+}
