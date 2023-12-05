cb7419d53d068b1911aacbdff362f3b10535f95d
aniqatc <aniqatc97@gmail.com>
2023-12-04 19:07:33 -0500

    day 066: initial markup & styling

---
+++ entries/066/README.md
@@ -0,0 +1,9 @@
+## Day 066
+
+**<a href="https://css100.aniqa.dev#day-066">Live Demo</a>**
+
+**Time to Complete**: 15 mins
+
+**Key Concepts**: trigger transitions on checked state of checkbox, setting different positions to elements around a specified point
+
+**Notes**:
---
+++ entries/066/content.js
@@ -0,0 +1,33 @@
+export function getMarkup() {
+	return `
+<!-- Day 066 -->
+<div class="day-066 frame" id="day-066">
+	<span class="code-link">
+		<a
+			href="https://github.com/aniqatc/css-100/tree/main/entries/066"
+			target="_blank"
+			rel="noreferrer"
+			>Day 066: Sparkle Checkbox</a
+		>
+	</span>
+    	<div class="center">
+		<input type="checkbox" id="sparkle-checkbox" />
+		<label for="sparkle-checkbox" class="sparkle-label"></label>
+		<div class="sparkles center">
+			<span class="sparkle"></span>
+			<span class="sparkle"></span>
+			<span class="sparkle"></span>
+			<span class="sparkle"></span>
+			<span class="sparkle"></span>
+			<span class="sparkle"></span>
+			<span class="sparkle"></span>
+			<span class="sparkle"></span>
+			<span class="sparkle"></span>
+			<span class="sparkle"></span>
+			<span class="sparkle"></span>
+			<span class="sparkle"></span>
+		</div>
+	</div>
+</div>  
+`;
+}
