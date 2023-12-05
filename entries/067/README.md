5b4c3abc0a752327537399cb467cd3d2fd0d2cfd
aniqatc <aniqatc97@gmail.com>
2023-12-05 16:31:17 -0500

    day 067: initial markup & styling

---
+++ entries/067/README.md
@@ -0,0 +1,9 @@
+## Day 067
+
+**<a href="https://css100.aniqa.dev#day-067">Live Demo</a>**
+
+**Time to Complete**: 1 hour
+
+**Key Concepts**: 3D transformations, absolute positioning
+
+**Notes**:
---
+++ entries/067/content.js
@@ -0,0 +1,51 @@
+export function getMarkup() {
+	return `
+<!-- Day 067 -->
+<div class="day-067 frame" id="day-067">
+    <span class="code-link">
+		<a
+			href="https://github.com/aniqatc/css-100/tree/main/entries/067"
+			target="_blank"
+		    rel="noreferrer"
+			>Day 067: 3D Parasol</a
+		>
+	</span>
+    <div class="inner-frame">
+    <div class="parasol">
+		<div class="top">
+			<div class="triangle"></div>
+			<div class="triangle"></div>
+			<div class="triangle"></div>
+			<div class="triangle"></div>
+			<div class="triangle"></div>
+			<div class="triangle"></div>
+			<div class="triangle"></div>
+			<div class="triangle"></div>
+			<div class="triangle"></div>
+			<div class="triangle"></div>
+			<div class="triangle"></div>
+			<div class="triangle"></div>
+		</div>
+		<div class="pole"></div>
+		<div class="shadow">
+			<div class="shadow-top">
+				<div class="triangle"></div>
+				<div class="triangle"></div>
+				<div class="triangle"></div>
+				<div class="triangle"></div>
+				<div class="triangle"></div>
+				<div class="triangle"></div>
+				<div class="triangle"></div>
+				<div class="triangle"></div>
+				<div class="triangle"></div>
+				<div class="triangle"></div>
+				<div class="triangle"></div>
+				<div class="triangle"></div>
+			</div>
+			<div class="shadow-pole"></div>
+		</div>
+	</div>
+    </div>
+</div>  
+`;
+}
