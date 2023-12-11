02dc79ca6e502a22b344c0f693cb7ae77571060e
aniqatc <aniqatc97@gmail.com>
2023-12-10 22:31:48 -0500

    day 069: add markup & styling

---
+++ entries/069/README.md
@@ -0,0 +1,9 @@
+## Day 069
+
+**<a href="https://css100.aniqa.dev#day-069">Live Demo</a>**
+
+**Time to Complete**: 25 mins
+
+**Key Concepts**: border-radius for eye shape, border-width for pupil, translations for eye movement and eye blink
+
+**Notes**:
---
+++ entries/069/content.js
@@ -0,0 +1,22 @@
+export function getMarkup() {
+	return `
+			<!-- Day 069 --> 
+			<div class="day-069 frame" id="day-069">
+				<span class="code-link">
+					<a
+						href="https://github.com/aniqatc/css-100/tree/main/entries/069"
+						target="_blank"
+						rel="noreferrer"
+						>Day 069: Blinking Eye</a
+					>
+				</span>
+				<div class="center">
+					<div class="top lid"></div>
+		                <div class="eye">
+			                <div class="pupil"></div>
+		                </div>
+		            <div class="bottom lid"></div>
+				</div>
+			</div>  
+    `;
+}
