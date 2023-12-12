edbf758b3db19c7a504baec6efb1488317139d15
aniqatc <aniqatc97@gmail.com>
2023-12-12 02:19:32 -0500

    day 072: markup & styling

---
+++ entries/072/README.md
@@ -0,0 +1,9 @@
+## Day 072
+
+**<a href="https://css100.aniqa.dev#day-072">Live Demo</a>**
+
+**Time to Complete**: 15 mins
+
+**Key Concepts**: staggered animations with animation-delay
+
+**Notes**:
---
+++ entries/072/content.js
@@ -0,0 +1,25 @@
+export function getMarkup() {
+	return `
+    	<!-- Day 072 -->
+			<div class="day-072 frame" id="day-072">
+				<span class="code-link">
+					<a
+						href="https://github.com/aniqatc/css-100/tree/main/entries/072"
+						target="_blank"
+						rel="noreferrer"
+						>Day 072: Rings</a
+					>
+				</span>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+			</div>  
+    `;
+}
