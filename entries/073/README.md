85698ef47ba2eeb11e6fc96c550223105ac635cf
aniqatc <aniqatc97@gmail.com>
2023-12-12 14:25:20 -0500

    day 073: markup and styling

---
+++ entries/073/README.md
@@ -0,0 +1,9 @@
+## Day 073
+
+**<a href="https://css100.aniqa.dev#day-073">Live Demo</a>**
+
+**Time to Complete**: 20 mins
+
+**Key Concepts**: word carousel with overflow and transform property
+
+**Notes**:
---
+++ entries/073/content.js
@@ -0,0 +1,27 @@
+export function getMarkup() {
+	return `
+    	<!-- Day 073 -->
+			<div class="day-073 frame" id="day-073">
+				<span class="code-link">
+					<a
+						href="https://github.com/aniqatc/css-100/tree/main/entries/073"
+						target="_blank"
+						rel="noreferrer"
+						>Day 073: Word Carousel</a
+					>
+				</span>
+	<div class="center">
+		<div class="carousel">
+			<div>I'm a </div>
+			<div class="cycle">
+				<span class="word">Panther</span>
+				<span class="word">Leopard</span>
+				<span class="word">Tiger</span>
+				<span class="word">Jaguar</span>
+				<span class="word">Panther</span>
+			</div>
+		</div>
+	</div>
+</div>  
+    `;
+}
