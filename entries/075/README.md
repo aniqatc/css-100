5ff635890a78fc52341b92c07b4e2e57409c9579
aniqatc <aniqatc97@gmail.com>
2023-12-20 03:37:36 -0500

    day 075: markup & styling

---
+++ entries/075/README.md
@@ -0,0 +1,9 @@
+## Day 075
+
+**<a href="https://css100.aniqa.dev#day-075">Live Demo</a>**
+
+**Time to Complete**: 25 mins
+
+**Key Concepts**: 3d rotations
+
+**Notes**:
---
+++ entries/075/content.js
@@ -0,0 +1,25 @@
+export function getMarkup() {
+	return `
+    	<!-- Day 075 -->
+			<div class="day-075 frame" id="day-075">
+				<span class="code-link">
+					<a
+						href="https://github.com/aniqatc/css-100/tree/main/entries/075"
+						target="_blank"
+						rel="noreferrer"
+						>Day 075: Spinning Disc</a
+					>
+				</span>
+				<div class="center">
+                    <div class="disc"></div>
+                    <div class="disc"></div>
+                    <div class="disc"></div>
+                    <div class="disc"></div>
+                    <div class="disc"></div>
+                    <div class="disc"></div>
+                    <div class="disc"></div>
+                    <div class="disc"></div>
+				</div>
+			</div>  
+    `;
+}
