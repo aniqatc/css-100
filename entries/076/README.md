c4361bc7de4a7b22fec89b44d614a23584b33b10
aniqatc <aniqatc97@gmail.com>
2023-12-26 21:06:23 -0500

    day 076: markup & styling

---
+++ entries/076/README.md
@@ -0,0 +1,9 @@
+## Day 076
+
+**<a href="https://css100.aniqa.dev#day-076">Live Demo</a>**
+
+**Time to Complete**: 30 mins
+
+**Key Concepts**: form :valid selector, backface visibility with 3d transformations
+
+**Notes**:
---
+++ entries/076/content.js
@@ -0,0 +1,22 @@
+export function getMarkup() {
+	return `
+<!-- Day 076 -->
+			<div class="day-076 frame" id="day-076">
+				<span class="code-link">
+					<a
+						href="https://github.com/aniqatc/css-100/tree/main/entries/076"
+						target="_blank"
+						rel="noreferrer"
+						>Day 076: Secret Key</a
+					>
+				</span>
+				<div class="center">
+					<form class="form-container">
+			            <input type="text" class="input-box" placeholder='Enter "password"' pattern="[pP]assword" required />
+			            <div class="key-title">Secret Key</div>
+		            </form>
+		            <div class="key-answer">MD5-SU3-CX8</div>
+				</div>
+</div>  
+`;
+}
