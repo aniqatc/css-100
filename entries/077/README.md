7c95b18710062d8945d02695a0747e0dd6b606b3
aniqatc <aniqatc97@gmail.com>
2024-01-03 22:09:40 -0500

    day 077: initial markup & styling

---
+++ entries/077/README.md
@@ -0,0 +1,9 @@
+## Day 077
+
+**<a href="https://css100.aniqa.dev#day-077">Live Demo</a>**
+
+**Time to Complete**: 15 mins
+
+**Key Concepts**: @for loop for animations
+
+**Notes**:
---
+++ entries/077/content.js
@@ -0,0 +1,25 @@
+export function getMarkup() {
+	return `
+<!-- Day 077 -->
+<div class="day-077 frame" id="day-077">
+	<span class="code-link">
+		<a
+		href="https://github.com/aniqatc/css-100/tree/main/entries/077"
+	    target="_blank"
+		rel="noreferrer"
+		>Day 077: Motion Blur</a
+		>
+    </span>
+	<div class="center">
+		<div class="ball">
+			<div class="blur blur-1"></div>
+			<div class="blur blur-2"></div>
+			<div class="blur blur-3"></div>
+			<div class="blur blur-4"></div>
+			<div class="blur blur-5"></div>
+			<div class="blur blur-6"></div>
+		</div>
+	</div>
+</div>  
+    `;
+}
