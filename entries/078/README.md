5e8cc14a2870818ba3de4241fb7423291259a294
aniqatc <aniqatc97@gmail.com>
2024-01-03 22:21:49 -0500

    day 078: initial markup & styling

---
+++ entries/078/README.md
@@ -0,0 +1,9 @@
+## Day 078
+
+**<a href="https://css100.aniqa.dev#day-078">Live Demo</a>**
+
+**Time to Complete**: 10 mins
+
+**Key Concepts**: shadows on hover
+
+**Notes**:
---
+++ entries/078/content.js
@@ -0,0 +1,20 @@
+export function getMarkup() {
+	return `
+<!-- Day 078 -->
+<div class="day-078 frame" id="day-078">
+	<span class="code-link">
+		<a
+		href="https://github.com/aniqatc/css-100/tree/main/entries/078"
+	    target="_blank"
+		rel="noreferrer"
+		>Day 078: Hover Shadow</a
+		>
+    </span>
+	<div class="center">
+		<div class="button">Hover Me</div>
+		<div class="button">Hover Me</div>
+		<div class="button">Hover Me</div>
+	</div>
+</div>  
+    `;
+}
