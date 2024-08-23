194493400c5db0a5563cb44f29d530f5ea0f38bf
aniqatc <aniqatc97@gmail.com>
2024-08-23 01:45:03 -0400

    day 080: markup & styling

---
+++ entries/080/README.md
@@ -0,0 +1,9 @@
+## Day 080
+
+**<a href="https://css100.aniqa.dev#day-080">Live Demo</a>**
+
+**Time to Complete**: 30 mins
+
+**Key Concepts**: flex shorthand, animating flexbox with width & transition properties
+
+**Notes**:
---
+++ entries/080/content.js
@@ -0,0 +1,28 @@
+export function getMarkup() {
+	return `
+<!-- Day 080 -->
+<div class="day-080 frame" id="day-080">
+	<span class="code-link">
+		<a
+		href="https://github.com/aniqatc/css-100/tree/main/entries/080"
+	    target="_blank"
+		>Day 080: Flexbox Accordion</a
+		>
+    </span>
+        <div class="center">
+            <div class="panel">
+                <span class="text">First</span>
+            </div>
+            <div class="panel">
+                <span class="text">Second</span>
+            </div>
+            <div class="panel">
+                <span class="text">Third</span>
+            </div>
+            <div class="panel">
+                <span class="text">Fourth</span>
+            </div>
+        </div>
+    </div>  
+    `;
+}
