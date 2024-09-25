2cf89b78afd6a3cac20016f6f5af019a03d10a82
aniqatc <aniqatc97@gmail.com>
2024-09-24 21:40:26 -0400

    day 081: markup, styling

---
+++ entries/081/README.md
@@ -0,0 +1,11 @@
+## Day 080
+
+**<a href="https://css100.aniqa.dev#day-081">Live Demo</a>**
+
+**Time to Complete**: 1 hour
+
+**Key Concepts**: keyframe animations
+
+**Notes**:
+
+https://www.the-art-of-web.com/css/bouncing-ball-animation/
---
+++ entries/081/content.js
@@ -0,0 +1,20 @@
+export function getMarkup() {
+	return `
+<!-- Day 081 -->
+<div class="day-081 frame" id="day-081">
+	<span class="code-link">
+		<a
+		href="https://github.com/aniqatc/css-100/tree/main/entries/081"
+	    target="_blank"
+		rel="noreferrer"
+		>Day 081: Bouncing Ball</a
+		>
+    </span>
+	    <div class="top">
+		    <div class="outer">
+			    <div class="ball"></div>
+		    </div>
+	    </div>
+    </div>  
+    `;
+}
